dca97c432a1bd873ef6159c99577234bfb7342dd
Rishabh Joshi <rishabh.joshi2103@gmail.com>
2026-02-11 10:04:20 +0530

    Data Transformation

--- src/logger.py
+++ src/logger.py
@@ -36,6 +36,26 @@
 
 
 
+# import logging
+# import os
+# from datetime import datetime
+
+# LOG_DIR = "logs"
+# os.makedirs(LOG_DIR, exist_ok=True)
+
+
+# LOG_FILE_NAME = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+# LOG_FILE_PATH = os.path.join(LOG_DIR, LOG_FILE_NAME)
+
+# logging.basicConfig(
+#     filename=LOG_FILE_PATH,
+#     format= "[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+#     level=logging.INFO
+# )
+
+# logger = logging.getLogger()
+
+
 import logging
 import os
 from datetime import datetime
@@ -43,14 +63,15 @@ from datetime import datetime
 LOG_DIR = "logs"
 os.makedirs(LOG_DIR, exist_ok=True)
 
-
-LOG_FILE_NAME = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
-LOG_FILE_PATH = os.path.join(LOG_DIR, LOG_FILE_NAME)
-
-logging.basicConfig(
-    filename=LOG_FILE_PATH,
-    format= "[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
-    level=logging.INFO
+LOG_FILE = os.path.join(
+    LOG_DIR,
+    datetime.now().strftime("%m_%d_%Y_%H_%M_%S.log")
 )
 
-logger = logging.getLogger()
+logging.basicConfig(
+    filename=LOG_FILE,
+    format="%(asctime)s %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
+)
+
+logger = logging.getLogger("mlproject")
